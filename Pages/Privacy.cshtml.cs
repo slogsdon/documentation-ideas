422ff22d4dfac18f87e7e3d4ae368555808c4862
Shane Logsdon <shane@shanelogsdon.com>
2019-08-06 23:45:23 -0400

    add models back in as they are required

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Documentation.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
